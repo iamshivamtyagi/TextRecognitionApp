574214a81cf5a1a135a1184277e2bb09768b2906
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-10-06 16:00:30 +0530

    updated permissions

--- app/src/main/java/com/example/textrecognitionapp/MainActivity.java
+++ app/src/main/java/com/example/textrecognitionapp/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.textrecognitionapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -40,7 +44,12 @@ public class MainActivity extends AppCompatActivity {
         mPreviewIv = findViewById(R.id.imageIv);
 
         //camera permission
+        cameraPermission = new String{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
+        //storage permission
+        storagePermission = new String{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        } ;
 
     }
 
@@ -98,18 +107,45 @@ public class MainActivity extends AppCompatActivity {
             }
 
             private void pickGallery() {
+                //intent to pick image from gallery
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                //set intent type to image
+                intent.setType("image/*");
+                startActivityForResult(intent,IMAGE_PICK_GALLERY_CODE);
             }
 
             private void requestStoragePermission() {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        storagePermission, STORAGE_REQUEST_CODE);
             }
 
             private void pickCamera() {
+                //intent to take image from camera, it will also be save to storage to get high quality image
+                ContentValues values = new ContentValues();
+                //title of image
+                values.put(MediaStore.Images.Media.TITLE,"NewPic");
+                //description
+                values.put(MediaStore.Images.Media.DESCRIPTION,"Image to text");
+                image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+                startActivityForResult(cameraIntent,IMAGE_PICK_CAMERA_CODE);
+
             }
 
             private boolean checkStoragePermission() {
+
+                boolean resultStorage = ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+
+                return resultStorage;
             }
 
             private void requestCameraPermission() {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        cameraPermission, CAMERA_REQUEST_CODE);
+
             }
 
             private boolean checkCameraPermission() {
@@ -118,16 +154,17 @@ public class MainActivity extends AppCompatActivity {
                before inserting to image view that's why storage permission also be required
                 */
 
-                boolean resultCam = ContextCompat.checkSelfPermission(this,
+                boolean resultCam = ContextCompat.checkSelfPermission(MainActivity.this,
                         Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
 
-                boolean resultStorage = ContextCompat.checkSelfPermission(this,
+                boolean resultStorage = ContextCompat.checkSelfPermission(MainActivity.this,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
 
                 return resultCam && resultStorage;
             }
         });
     }
+
 
 
 }
